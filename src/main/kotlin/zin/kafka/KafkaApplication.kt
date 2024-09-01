a850a64203f533e04785c68b0aea86f5f3af01cb
iamzin <eunzin.park@gmail.com>
2024-09-01 14:47:53 +0900

    init

---
+++ src/main/kotlin/zin/kafka/KafkaApplication.kt
@@ -0,0 +1,11 @@
+package zin.kafka
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KafkaApplication
+
+fun main(args: Array<String>) {
+    runApplication<KafkaApplication>(*args)
+}
